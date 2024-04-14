336c8cefd937864104f0e4ab18c99e1fae128e92
Geyer Artúr <artur.geyer@proton.me>
2024-04-14 18:50:52 +0200

    Started Adding Boilerplate Code

---
+++ app/core/dealer.py
@@ -0,0 +1,21 @@
+class Dealer:
+    def __init__(self) -> None:
+        pass
+
+    def inspectCards():
+        pass
+
+    def getCard():
+        pass
+
+    def hit():
+        pass
+
+    def stand():
+        pass
+
+    def doubleDown():
+        pass
+
+    def split():
+        pass
---
+++ app/core/player.py
@@ -0,0 +1,21 @@
+class Player:
+    def __init__(self) -> None:
+        pass
+
+    def inspectCards():
+        pass
+
+    def getCard():
+        pass
+
+    def hit():
+        pass
+
+    def stand():
+        pass
+
+    def doubleDown():
+        pass
+
+    def split():
+        pass
